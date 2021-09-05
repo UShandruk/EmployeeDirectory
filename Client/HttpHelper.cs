cdcb6e5624c91fbb10a5e6f75f27f47215bf483e
UShandruk <ulyana.shandruk@gmail.com>
2021-09-05 15:50:06 +0300

    Серверное приложение должно быть API, соот-но, пересоздала его с шаблоном для API

--- Client/HttpHelper.cs
+++ Client/HttpHelper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,174 +14,173 @@ namespace Client
     /// Вспомогательный код из п.1 подсказок к выполнению задания:
     /// https://gist.github.com/MaximKsh/acd77fc0352361a1fed64d146a694cf5
     /// </summary>
-    class HttpHelper
+
+    /// <summary>
+    /// Класс предназначен для хранения информации о результате HTTP запроса.
+    /// </summary>
+    public class WebServiceResponse
     {
         /// <summary>
-        /// Класс предназначен для хранения информации о результате HTTP запроса.
+        /// HTTP код ответа
         /// </summary>
-        public class WebServiceResponse
-        {
-            /// <summary>
-            /// HTTP код ответа
-            /// </summary>
-            public HttpStatusCode StatusCode => this.HttpResponse.StatusCode;
+        public HttpStatusCode StatusCode => this.HttpResponse.StatusCode;
 
-            /// <summary>
-            /// Результат выполнения HTTP запроса
-            /// </summary>
-            public HttpResponseMessage HttpResponse { get; set; }
+        /// <summary>
+        /// Результат выполнения HTTP запроса
+        /// </summary>
+        public HttpResponseMessage HttpResponse { get; set; }
+    }
+
+    /// <summary>
+    /// Класс предназначен для хранения типизированной информации о результате HTTP запроса.
+    /// </summary>
+    /// <typeparam name="T">Тип объекта в HTTP-ответе.</typeparam>
+    public class WebServiceResponse<T> : WebServiceResponse
+    {
+        /// <summary>
+        /// Объект из ответа на запрос.
+        /// </summary>
+        public T Result { get; set; }
+    }
+
+    /// <summary>
+    /// Вспомогательные функции для работы с HTTP
+    /// </summary>
+    public static class HttpHelper
+    {
+        /// <summary>
+        /// Выполнить GET запрос.
+        /// </summary>
+        /// <param name="client">Объект HttpClient</param>
+        /// <param name="url">Адрес, по которому выполняется запрос</param>
+        /// <returns></returns>
+        public static async Task<WebServiceResponse> RequestGetAsync(
+            HttpClient client,
+            string url)
+        {
+            // Выполнение запроса с помощью HttpClient
+            var response = await client.GetAsync(url);
+
+            // Возвращение результата
+            return new WebServiceResponse
+            {
+                HttpResponse = response
+            };
         }
 
         /// <summary>
-        /// Класс предназначен для хранения типизированной информации о результате HTTP запроса.
+        /// Выполнить GET запрос и получить типизированный результат
         /// </summary>
-        /// <typeparam name="T">Тип объекта в HTTP-ответе.</typeparam>
-        public class WebServiceResponse<T> : WebServiceResponse
+        /// <param name="client">Объект HttpClient</param>
+        /// <param name="url">Адрес, по которому выполняется запрос</param>
+        /// <typeparam name="T">Тип объекта в ответе</typeparam>
+        /// <returns></returns>
+        public static async Task<WebServiceResponse<T>> RequestGetAsync<T>(
+            HttpClient client,
+            string url) where T : class
         {
-            /// <summary>
-            /// Объект из ответа на запрос.
-            /// </summary>
-            public T Result { get; set; }
+            // Выполнение запроса с помощью HttpClient
+            var response = await client.GetAsync(url);
+
+            // Если ответ не пуст, то выполняется десериализация содержимого из JSON в C# объект
+            T result = null;
+            if (response.Content.Headers.ContentLength != 0)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<T>(responseString);
+            }
+
+            // Возвращение результата
+            return new WebServiceResponse<T>
+            {
+                Result = result,
+                HttpResponse = response
+            };
         }
 
         /// <summary>
-        /// Вспомогательные функции для работы с HTTP
+        /// Выполнить POST запрос.
         /// </summary>
-        public static class HttpHelper
+        /// <param name="client">Объект HttpClient</param>
+        /// <param name="url">Адрес, по которому выполняется запрос</param>
+        /// <param name="payloadObjects">Объекты, которые необходимо поместить в запрос</param>
+        /// <returns></returns>
+        public static async Task<WebServiceResponse> RequestPostAsync(
+            HttpClient client,
+            string url,
+            params object[] payloadObjects)
         {
-            /// <summary>
-            /// Выполнить GET запрос.
-            /// </summary>
-            /// <param name="client">Объект HttpClient</param>
-            /// <param name="url">Адрес, по которому выполняется запрос</param>
-            /// <returns></returns>
-            public static async Task<WebServiceResponse> RequestGetAsync(
-                HttpClient client,
-                string url)
+            // Необходимо правильно определить нагрузку запроса - это одиночный объект {...},
+            // список объектов [{...}, {...}], либо объектов вообще нет и тело POST запроса должно быть пустым
+            object payload;
+            switch (payloadObjects.Length)
             {
-                // Выполнение запроса с помощью HttpClient
-                var response = await client.GetAsync(url);
-
-                // Возвращение результата
-                return new WebServiceResponse
-                {
-                    HttpResponse = response
-                };
+                case 0:
+                    payload = string.Empty;
+                    break;
+                case 1:
+                    payload = payloadObjects[0];
+                    break;
+                default:
+                    payload = payloadObjects;
+                    break;
             }
 
-            /// <summary>
-            /// Выполнить GET запрос и получить типизированный результат
-            /// </summary>
-            /// <param name="client">Объект HttpClient</param>
-            /// <param name="url">Адрес, по которому выполняется запрос</param>
-            /// <typeparam name="T">Тип объекта в ответе</typeparam>
-            /// <returns></returns>
-            public static async Task<WebServiceResponse<T>> RequestGetAsync<T>(
-                HttpClient client,
-                string url) where T : class
+            // Выполнение сериализации из C# объекта в JSON
+            var payloadString = JsonConvert.SerializeObject(payload);
+
+            // Выполнение кодирования.
+            var payloadBytes = Encoding.UTF8.GetBytes(payloadString);
+            var content = new ByteArrayContent(payloadBytes);
+
+            // Установка служебной информации в HTTP заголовки.
+            // Это необходимо для того, чтобы сервер мог корректно интерпретировать содержимое запроса
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            content.Headers.ContentEncoding.Add("UTF-8");
+            content.Headers.ContentLength = payloadBytes.Length;
+
+            // Выполняем POST запрос
+            var response = await client.PostAsync(url, content);
+
+            // Возвращаем результат
+            return new WebServiceResponse
             {
-                // Выполнение запроса с помощью HttpClient
-                var response = await client.GetAsync(url);
+                HttpResponse = response
+            };
+        }
 
-                // Если ответ не пуст, то выполняется десериализация содержимого из JSON в C# объект
-                T result = null;
-                if (response.Content.Headers.ContentLength != 0)
-                {
-                    var responseString = await response.Content.ReadAsStringAsync();
-                    result = JsonConvert.DeserializeObject<T>(responseString);
-                }
+        /// <summary>
+        /// Выполнить POST запрос и получить типизированный результат
+        /// </summary>
+        /// <param name="client">Объект HttpClient</param>
+        /// <param name="url">Адрес, по которому выполняется запрос</param>
+        /// <param name="payloadObjects">Объекты, которые необходимо поместить в запрос</param>
+        /// <typeparam name="T">Тип объекта в ответе</typeparam>
+        /// <returns></returns>
+        public static async Task<WebServiceResponse<T>> RequestPostAsync<T>(
+            HttpClient client,
+            string url,
+            params object[] payloadObjects) where T : class
+        {
+            // Во избежание дублирования кода воспользуемся похожим методом
+            var rawResponse = await RequestPostAsync(client, url, payloadObjects);
+            var response = rawResponse.HttpResponse;
 
-                // Возвращение результата
-                return new WebServiceResponse<T>
-                {
-                    Result = result,
-                    HttpResponse = response
-                };
+            // Если ответ не пуст, то выполняется десериализация содержимого из JSON в C# объект
+            T result = null;
+            if (response.Content.Headers.ContentLength != 0)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<T>(responseString);
             }
 
-            /// <summary>
-            /// Выполнить POST запрос.
-            /// </summary>
-            /// <param name="client">Объект HttpClient</param>
-            /// <param name="url">Адрес, по которому выполняется запрос</param>
-            /// <param name="payloadObjects">Объекты, которые необходимо поместить в запрос</param>
-            /// <returns></returns>
-            public static async Task<WebServiceResponse> RequestPostAsync(
-                HttpClient client,
-                string url,
-                params object[] payloadObjects)
+            // Возвращение результата
+            return new WebServiceResponse<T>
             {
-                // Необходимо правильно определить нагрузку запроса - это одиночный объект {...},
-                // список объектов [{...}, {...}], либо объектов вообще нет и тело POST запроса должно быть пустым
-                object payload;
-                switch (payloadObjects.Length)
-                {
-                    case 0:
-                        payload = string.Empty;
-                        break;
-                    case 1:
-                        payload = payloadObjects[0];
-                        break;
-                    default:
-                        payload = payloadObjects;
-                        break;
-                }
-
-                // Выполнение сериализации из C# объекта в JSON
-                var payloadString = JsonConvert.SerializeObject(payload);
-
-                // Выполнение кодирования.
-                var payloadBytes = Encoding.UTF8.GetBytes(payloadString);
-                var content = new ByteArrayContent(payloadBytes);
-
-                // Установка служебной информации в HTTP заголовки.
-                // Это необходимо для того, чтобы сервер мог корректно интерпретировать содержимое запроса
-                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                content.Headers.ContentEncoding.Add("UTF-8");
-                content.Headers.ContentLength = payloadBytes.Length;
-
-                // Выполняем POST запрос
-                var response = await client.PostAsync(url, content);
-
-                // Возвращаем результат
-                return new WebServiceResponse
-                {
-                    HttpResponse = response
-                };
-            }
-
-            /// <summary>
-            /// Выполнить POST запрос и получить типизированный результат
-            /// </summary>
-            /// <param name="client">Объект HttpClient</param>
-            /// <param name="url">Адрес, по которому выполняется запрос</param>
-            /// <param name="payloadObjects">Объекты, которые необходимо поместить в запрос</param>
-            /// <typeparam name="T">Тип объекта в ответе</typeparam>
-            /// <returns></returns>
-            public static async Task<WebServiceResponse<T>> RequestPostAsync<T>(
-                HttpClient client,
-                string url,
-                params object[] payloadObjects) where T : class
-            {
-                // Во избежание дублирования кода воспользуемся похожим методом
-                var rawResponse = await RequestPostAsync(client, url, payloadObjects);
-                var response = rawResponse.HttpResponse;
-
-                // Если ответ не пуст, то выполняется десериализация содержимого из JSON в C# объект
-                T result = null;
-                if (response.Content.Headers.ContentLength != 0)
-                {
-                    var responseString = await response.Content.ReadAsStringAsync();
-                    result = JsonConvert.DeserializeObject<T>(responseString);
-                }
-
-                // Возвращение результата
-                return new WebServiceResponse<T>
-                {
-                    Result = result,
-                    HttpResponse = response
-                };
-            }
+                Result = result,
+                HttpResponse = response
+            };
         }
     }
+
 }
