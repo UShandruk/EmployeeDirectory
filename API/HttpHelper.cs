5d61350f72784b8006ed845ad33335760255d41d
UShandruk <ulyana.shandruk@gmail.com>
2021-09-06 08:19:25 +0300

    Удален ненужный контроллер, переименовано пространство имен

--- API/HttpHelper.cs
+++ API/HttpHelper.cs
@@ -8,7 +8,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Client
+namespace API.Controllers
 {
     /// <summary>
     /// Вспомогательный код из п.1 подсказок к выполнению задания:
